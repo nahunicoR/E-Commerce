aa41e1b4bf78e3124f94d92ff8ef8518de5a4b9f
jesus <m6nuel@gmail.com>
2022-12-01 20:51:00 -0300

    modularice la peticion POST

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,26 +9,28 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const createProduct = require('./createProduct');
 
+router.use('/product', createProduct);
 
 router.get('/', async (req,res,next) => {
 res.json('grupo 9')
 });
 
-router.get('/:id', async(req,res,next) => {
+// router.get('/:id', async(req,res,next) => {
     
 
-});
+// });
 
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
 
-});
+// });
 
-router.put('/', async (req, res) => {
+// router.put('/', async (req, res) => {
 
-});
+// });
 
-router.delete('/', async (req, res) => {
-});
+// router.delete('/', async (req, res) => {
+// });
 
 module.exports = router;
