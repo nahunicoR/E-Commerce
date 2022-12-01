5280bf28b23bb4ce092f827783bf61197adcb900
nahuel <nahunico.r@gmail.com>
2022-12-01 20:44:05 -0300

    agregue prop al model User

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -4,7 +4,30 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('user', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
