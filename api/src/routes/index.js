bbe690f2c0b6b4642298356502d7d68b9a54d779
wzseba <wzseba@gmail.com>
2022-12-01 21:33:22 -0300

    se agrego ruta para obtener todos los products

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,34 +1,44 @@
 const { Router } = require('express');
+const {Product} = require('../db');
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get('/products', async (req,res) => {
 
+    try {
 
-router.get('/', async (req,res,next) => {
+        let infoDB = await Product.findAll();
+        infoDB.length === 0 ? res.json('LA BASE DE DATOS ESTA VACIA') : res.json(infoDB);
+
+    } catch (error) {
+        return res.send(error.parent.detail);
+    }
 
 });
 
-router.get('/:id', async(req,res,next) => {
+router.get('/:id', async(req,res) => {
     
 
 });
 
-router.post('/', async (req, res) => {
-
+router.post('/createProduct', async (req, res) => {
+  
 });
 
 router.put('/', async (req, res) => {
 
 });
 
-router.delete('/', async (req, res) => {
+router.delete('/:id', async (req, res) => {
+
+
 });
+
 
 module.exports = router;
