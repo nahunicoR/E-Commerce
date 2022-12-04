3c4717f54f9caaf13572af11597f4d63271dffc3
jesus <m6nuel@gmail.com>
2022-12-04 19:51:08 -0300

    CRUD para Demo

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -23,7 +23,7 @@ module.exports = (sequelize) => {
       type: DataTypes.FLOAT,
     },
     category: {                  
-      type: DataTypes.ENUM('bombilla','mate','kit','yerba'),
+      type: DataTypes.ENUM('Bombilla','Mate','Kit','Yerba'),
     },
     description: {
       type: DataTypes.STRING
