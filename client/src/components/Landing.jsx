8de7d767f841b7f422584fa1ace4828ae72abd47
Silvia9680 <silviacarolina9680@gmail.com>
2022-12-05 20:34:42 -0300

    prueba

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React from 'react'
+import images from '../export/export'
+import './Landing.css';
+import { motion } from 'framer-motion'
 import { Button } from "@chakra-ui/button";
 import { Link } from "react-router-dom";
-import { Heading, Stack } from "@chakra-ui/layout";
+import {  Stack } from "@chakra-ui/layout";
 
-export default function Landing() {
-	return (
-		<div>
-			<Stack>
-				<Heading>Landing</Heading>
-
-				<Button w={"10%"}>
-					<Link to={"/home"}>Vamos a Matear</Link>
-				</Button>
-			</Stack>
-		</div>
-	);
+const Landing = () => {
+  return (
+	
+				<div>
+					<Stack>
+								
+						<Button w={"10%"}>
+							<Link to={"/home"}>Vamos a Matear</Link>
+						</Button>
+					</Stack>
+				
+			
+    <motion.div className='slider-container'>
+        <motion.div className='slider' drag='x' 
+        dragConstraints={{right: 0, left:-2123}} >
+        {images.map(image => (
+            <motion.div className='item'>
+                <img src={image} alt="" />
+            </motion.div>
+        ))}
+        </motion.div>
+        
+    </motion.div>
+	</div>
+  )
 }
+
+export default Landing
+
+
+
+
+
+
+
+
+
+// 
+// 	
+// 
