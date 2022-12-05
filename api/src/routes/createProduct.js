29ae1639bfa71c9c7a012b17e18e9d7041fcf4fa
jesus <m6nuel@gmail.com>
2022-12-05 04:45:25 -0300

    cambie create por findOrCreate

--- api/src/routes/createProduct.js
+++ api/src/routes/createProduct.js
@@ -5,13 +5,32 @@ const router = Router();
 router.post('/', async (req, res) => {
     try {
         let { title, price, category, description, image } = req.body;
-        let newProduct = await Product.create({
+
+        if(!title) return res.status(400).json('No se ingreso titulo');
+        if(!price) return res.status(400).json('No se ingreso precio');
+        if(!category) return res.status(400).json('No se ingreso categoria');
+        if(!description) return res.status(400).json('No se ingreso descripcion');
+        if(!image) return res.status(400).json('No se ingreso imagen');
+
+        const newProduct = {
             title,
             price,
             category,
             description,
             image
-        })
+        }
+
+        const [product, created] = await Product.findOrCreate({
+            where: {
+                title,
+                price,
+                category,
+                description,
+                image
+            },
+            defaults: newProduct
+        });
+        if(!created) return res.status(400).json('El producto ya existe');
         res.status(200).json(newProduct);
     } catch (error) {
         console.log(error)
