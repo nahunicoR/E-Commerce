c12ee8dab825a7baf569bf565d8074d65bdca5bf
wzseba <wzseba@gmail.com>
2022-12-07 18:50:55 -0300

    agregando propiedades a User

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -14,5 +14,20 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    lastName:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
   });
 };
