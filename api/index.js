93ca57d827d6e406a8bcd377d25e18cc0e59434f
AlexT-Dev <atacolmx@gmail.com>
2022-12-08 16:50:02 -0600

    Se actualiza index.js para levantar nube

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
+//Librerías para cloudinary
+const cloudinary = require("./cloudinary");
+const uploader = require("./multer");
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
+
+// Create Date: december 8, 2022
+// Author: Alejandro Téllez
+
+// Description: Para levantar el servidor
+server.get('/', (req, res) => {
+  res.send("Server cloudinary is running")
+});
+
+//Se genera la ruta del post
+server.post("/upload", uploader.single("file"), async (req, res) => {
+  const upload = await cloudinary.v2.uploader.upload(req.file.path);
+  return res.json({
+    success: true,
+    file: upload.secure_url,
+  });
+});
+
 
 // Syncing all the models at once.
 
