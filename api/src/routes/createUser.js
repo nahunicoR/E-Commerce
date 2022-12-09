350994b7d9963878d7269583e041dfeb3cc50888
wzseba <wzseba@gmail.com>
2022-12-09 15:25:28 -0300

    se agrego variables a la creacion de usuario

--- api/src/routes/createUser.js
+++ api/src/routes/createUser.js
@@ -5,9 +5,12 @@ const router = Router();
 
 router.post('/', async (req, res, next) => {
     try {
-        let { name } = req.body;
+        let { name, lastName, email, password } = req.body;
         let newUser = await User.create({
-            name
+            name,
+            lastName,
+            email,
+            password
         })
         res.status(200).json(newUser);
     } catch (error) {
