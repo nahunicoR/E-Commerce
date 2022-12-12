db6ca97d6281c132df5be3a7708317abf4c3eea4
Silvia9680 <silviacarolina9680@gmail.com>
2022-12-12 08:49:02 -0300

    Landing mejorada

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -26,30 +26,26 @@ const responsive = {
 export default function Landing() {
 	return (
 		<div>
-			<Stack>
-
-			    <Button 
-				// position={'absolute'}
-				top="50%"
-				left={"50%"}
-				size={"lg"}
-				 w={"10%"}>
+			<Stack position= {"relative"} height={"86hv"}>			
+				<Carousel responsive={responsive}>	
+		              <Image  boxSize='750px' objectFit='cover' 
+		                   src= {"https://yerbasara.files.wordpress.com/2017/04/mate3.jpg"} />
+		              <Image  boxSize='750px' objectFit='cover'
+		                   src= {"https://yerbasara.files.wordpress.com/2017/06/a59cd1f930d61534cacb05b3f9d19104cd2b19365e17063e72pimgpsh_fullsize_distr.png?w=661&h=334"}  />
+		              <Image  boxSize='750px' objectFit='cover'
+		                   src= {"https://s3.amazonaws.com/static.om.anigamy.net/static.biennatural.com.ar/App/Article/el-mate-una-infusion-exclusiva-de-los-argentinos-6272-mainImage-0.jpg"}  />
+		              <Image  boxSize='750px' objectFit='cover'
+		                   src= {"https://statics.forbesargentina.com/2021/06/crop/60d1e2c96474f__822x460.webp"} />
+	            </Carousel>
+	              <Button 
+				     colorScheme={"teal"}
+					 position={'absolute'}
+    				 top="45%"
+				     left={"44%"}
+				     size={"lg"}
+				     w={"15%"}>
 					<Link to={"/home"}>Vamos a Matear</Link>
-				</Button>		
-				
-				<Carousel responsive={responsive}>
-	
-		          <Image  boxSize='750px' objectFit='cover' 
-		               src= {"https://yerbasara.files.wordpress.com/2017/04/mate3.jpg"} />
-		          <Image  boxSize='750px' objectFit='cover'
-		               src= {"https://yerbasara.files.wordpress.com/2017/06/a59cd1f930d61534cacb05b3f9d19104cd2b19365e17063e72pimgpsh_fullsize_distr.png?w=661&h=334"}  />
-		          <Image  boxSize='750px' objectFit='cover'
-		               src= {"https://s3.amazonaws.com/static.om.anigamy.net/static.biennatural.com.ar/App/Article/el-mate-una-infusion-exclusiva-de-los-argentinos-6272-mainImage-0.jpg"}  />
-		          <Image  boxSize='750px' objectFit='cover'
-		                src= {"https://statics.forbesargentina.com/2021/06/crop/60d1e2c96474f__822x460.webp"} />
-	
-	
-                </Carousel>
+				   </Button>	
             </Stack>
 		</div>
 	);
