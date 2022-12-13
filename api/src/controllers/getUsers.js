93986cf4043d33310a2abb7363c4b3791ff6eb97
AlexT-Dev <atacolmx@gmail.com>
2022-12-13 12:59:10 -0600

    Ruta para todos los usuarios

--- api/src/controllers/getUsers.js
+++ api/src/controllers/getUsers.js
@@ -1,6 +1,10 @@
 
 const { User, Order } = require("../db");
 
+/* Date Creation: December 12, 2022
+   Author: Alejandro Téllez Aguilar
+   Description: Crea el servicio de la ruta /users/all para otener todos los usuarios
+*/
 const getUsers = async () => {
     try {
         const usersDb = await User.findAll();
