a4320e388e32a791cffb0a40e290d6e606db3226
jesus <m6nuel@gmail.com>
2022-12-22 15:49:39 -0300

    recuperando deploy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 //Librerías para cloudinary
-// const cloudinary = require("./cloudConfig");
-// const uploader = require("./multerCloud");
+const cloudinary = require("./cloudConfig");
+const uploader = require("./multerCloud");
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
@@ -11,23 +11,23 @@ const port = process.env.PORT || 3001;
 
 // Description: Para levantar el servidor
 // Probar en postman o navegador: http://localhost:3001/cloudinary
-// server.get('/cloudinary', (req, res) => {
-//   res.send(`%s listening at ${port}`)
-// });
+server.get('/cloudinary', (req, res) => {
+  res.send(`%s listening at ${port}`)
+});
 
 //Se genera la ruta del post
 //Para subir una imagen
-//  server.post("/cloudinary/upload", uploader.single("file"), async (req, res) => {
+ server.post("/cloudinary/upload", uploader.single("file"), async (req, res) => {
   
-//   const upload = await cloudinary.v2.uploader.upload(req.file.path);
+  const upload = await cloudinary.v2.uploader.upload(req.file.path);
  
  
-//   return res.json({
-//     success: true,
-//     file: upload.secure_url,
-//     data: "Imagen ha sido cargada",
-//   });
-// });
+  return res.json({
+    success: true,
+    file: upload.secure_url,
+    data: "Imagen ha sido cargada",
+  });
+});
 
 
 
@@ -35,11 +35,11 @@ const port = process.env.PORT || 3001;
 //Para varias imagenes en temporal
 // server.post("/cloudinary/upload", uploader.array("files"), uploadFiles);
 
-// function uploadFiles(req, res) {
-//     console.log(req.body);
-//     console.log(req.files);
-//     res.json({ message: "Successfully uploaded files" });
-// }
+function uploadFiles(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "Successfully uploaded files" });
+}
 
 
 // Syncing all the models at once.
