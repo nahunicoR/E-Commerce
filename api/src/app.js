c60f113a60a77c6978df2c3cc1dc9de0b9e1f22a
Silvia9680 <silviacarolina9680@gmail.com>
2022-12-22 21:45:25 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -20,6 +20,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
+   
 });
 
 server.use('/', routes);
