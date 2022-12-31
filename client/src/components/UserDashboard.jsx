7c8903c732f87266807a0ac86fe120f0d81532e6
William Salas <willbullet52@gmail.com>
2022-12-30 20:37:15 -0500

    feat(client):se modificó la apariencia del dashboard de usuario

--- client/src/components/UserDashboard.jsx
+++ client/src/components/UserDashboard.jsx
@@ -1,24 +1,146 @@
 import React from "react";
+import orders from "../odersMock.js";
+import {
+	HiOutlineChevronDoubleLeft,
+	HiOutlineChevronDoubleRight,
+} from "react-icons/hi";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
-import { Button, Avatar } from "@chakra-ui/react";
+import {
+	Button,
+	Avatar,
+	Box,
+	Flex,
+	Heading,
+	Text,
+	Tag,
+	Image,
+	IconButton,
+} from "@chakra-ui/react";
 
 export default function UserDashboard() {
 	const { user } = useAuth0();
 	const navigate = useNavigate();
+
+	const slideRight = () => {
+		var slider = document.getElementsByClassName("css-1peemgu");
+		console.log(slider);
+		slider.scrollRight = slider.scrollRight + 500;
+	};
+	const slideLeft = () => {
+		var slider = document.getElementsByClassName("css-1peemgu");
+		console.log(slider);
+		slider.scrollLeft = slider.scrollLeft + 500;
+	};
+
 	return (
-		<div>
-			<Button colorScheme={"teal"} onClick={() => navigate(-1)}>
-				volver
-			</Button>
-			<Avatar
-				size={"2xl"}
-				name={user.name}
-				src={user.picture}
-				alt="profile-snippet"
-			/>
-			<h1>Hola, {user.name}</h1>
-			<p>{JSON.stringify(user)}</p>
-		</div>
+		<>
+			<Box
+				gap={3}
+				display={"flex"}
+				flexDirection={"column"}
+				justifyContent={"center"}
+				h={"100%"}
+			>
+				<Button
+					w={"fit-content"}
+					m={"9"}
+					colorScheme={"teal"}
+					onClick={() => navigate(-1)}
+				>
+					volver
+				</Button>
+
+				<Flex
+					alignSelf={"center"}
+					justifyContent={"space-between"}
+					w={"70%"}
+					h={"10%"}
+					/* bg={"red.100"} */
+					padding={"5"}
+					border={"1px"}
+					borderColor={"gray.200"}
+					borderRadius={"md"}
+					bg={"white"}
+					alignItems={"center"}
+				>
+					<Flex gap={"9"} flexDir={"row"}>
+						<Avatar
+							size={"2xl"}
+							name={user.name}
+							src={user.picture}
+							alt="profile-snippet"
+						/>
+
+						<Flex gap={3} flexDirection={"column"}>
+							<Heading>{user.name}</Heading>
+							<Text color={"gray.400"}>{user.email}</Text>
+							<Tag
+								colorScheme={user.email_verified ? "green" : null}
+								w={"fit-content"}
+							>
+								{user.email_verified ? "Verified" : "Unverified"}
+							</Tag>
+						</Flex>
+					</Flex>
+					<Heading>info1</Heading>
+					<Heading>info 2</Heading>
+				</Flex>
+
+				<Flex
+					alignSelf={"center"}
+					justifyContent={"space-between"}
+					w={"70%"}
+					h={"fit-content"}
+					flexDirection={"column"}
+					padding={"8"}
+					border={"1px"}
+					borderColor={"gray.200"}
+					borderRadius={"md"}
+					bg={"white"}
+					alignItems={"center"}
+					gap={5}
+					overflow={"hidden"}
+				>
+					<Heading alignSelf={"flex-start"} size={"md"}>
+						My Orders
+					</Heading>
+					<Flex
+						gap={5}
+						flexDirection={"row"}
+						alignSelf={"flex-start"}
+						overflowX={"scroll"}
+						scrollBehavior={"smooth"}
+						whiteSpace={"nowrap"}
+					>
+						{orders.map((o) => {
+							return (
+								<Box
+									padding={"8"}
+									border={"1px"}
+									borderColor={"gray.200"}
+									borderRadius={"md"}
+									h={"230px"}
+									w={"230px"}
+									bg={"white"}
+								>
+									<Image src={o.image} />
+								</Box>
+							);
+						})}
+					</Flex>
+					<Flex gap={3}>
+						<IconButton
+							onClick={slideLeft}
+							icon={<HiOutlineChevronDoubleLeft />}
+						/>
+						<IconButton
+							onClick={slideRight}
+							icon={<HiOutlineChevronDoubleRight />}
+						/>
+					</Flex>
+				</Flex>
+			</Box>
+		</>
 	);
 }
---
+++ client/src/odersMock.js
@@ -0,0 +1,177 @@
+let orders = [
+	{
+		title: "Bombilla Económica",
+		price: "200",
+		category: "Bombilla",
+		description: "Bombilla económica de aluminio",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/c40d2093be97e34edab289e0cf0d6f15_orig_270x.progressive.jpg?v=1647031170",
+		material: "Industrial",
+		stock: 50,
+	},
+	{
+		title: "Bombilla para molienda fina",
+		price: "450",
+		category: "Bombilla",
+		description:
+			"Bombilla con base monilienda, aluminio y filtro de acero inoxidable",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/4ae619f439cb528cb523d561038f92c9_orig_270x.progressive.jpg?v=1647031688",
+		material: "Industrial",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Clásica",
+		price: "300",
+		category: "Bombilla",
+		description: "Bombilla clásica de acero inoxidable",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_6988_270x.progressive.jpg?v=1652861228",
+		material: "Industrial",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Clásica con punta dorada",
+		price: "500",
+		category: "Bombilla",
+		description: "Bombilla clásica de acero inoxidable con punta dorada",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_0647_270x.progressive.jpg?v=1669286847",
+		material: "Industrial",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Clásica Pico de Loro",
+		price: "500",
+		category: "Bombilla",
+		description: "Bombilla clásica de acero inoxidable con pico de loro",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_5370_270x.progressive.jpg?v=1647192240",
+		material: "Industrial",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Corta",
+		price: "320",
+		category: "Bombilla",
+		description: "Bombilla corta de aluminio, ideal para mate pequeño o taza",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_9957_270x.progressive.jpg?v=1665662310",
+		material: "Industrial",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Resorte",
+		price: "400",
+		category: "Bombilla",
+		description: "Bombilla de acero inoxidable con resorte",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/cd8baa631b14792b315caf1f46235207_orig_270x.progressive.jpg?v=1647038039",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Exclusiva Premium",
+		price: "570",
+		category: "Bombilla",
+		description:
+			"Bombilla de acero inoxidable con resorte y punta dorada, producto Premium",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_9983_270x.progressive.jpg?v=1665660210",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Bombilla Pico de Loro Exclusiva Premium",
+		price: "670",
+		category: "Bombilla",
+		description:
+			"Bombilla de acero inoxidable con resorte y pico de loro, producto Premium",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_7815_270x.progressive.jpg?v=1657112585",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Mate con base",
+		price: "500",
+		category: "Mate",
+		description: "Mate de madera con base artesanal",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/2248_270x.jpg",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Set Mate con Bombilla",
+		price: "850",
+		category: "Mate",
+		description: "Set de mate de madera con bombilla de acero inoxidable",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_6804_270x.progressive.jpg?v=1651842573",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Mate Torpedo Clásico",
+		price: "700",
+		category: "Mate",
+		description: "Mate de madera con base artesanal",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/912d34cf2c12e13df48c24a58768beaa_orig_270x.progressive.jpg?v=1660826980",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Mate Torpedo Premium Alpaca",
+		price: "900",
+		category: "Mate",
+		description: "Mate de alpaca con base artesanal cuero",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_6112_270x.progressive.jpg?v=1660905652",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Mate Torpedo Premium con base",
+		price: "1300",
+		category: "Mate",
+		description: "Mate excepcional color rojo",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/31bacc6f3f4a5f97d6ead06d3f118e6a_orig_270x.jpg",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Mate Torpedo Especial",
+		price: "1200",
+		category: "Mate",
+		description: "Mate especial color negro",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/ce54b53a68731cf694529284ba4d4c55_orig_270x.jpg",
+		material: "Sintetico",
+		stock: 50,
+	},
+	{
+		title: "Mate Torpedo Premium Cincelado con base",
+		price: "1500",
+		category: "Mate",
+		description: "Mate Torpedo Especial, cincelado color madera",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_6824_cdd1988c-a666-4ddb-865e-d0b024b200cd_270x.jpg",
+		material: "Artesanal",
+		stock: 50,
+	},
+	{
+		title: "Mate Torpedo Premium alto",
+		price: "1600",
+		category: "Mate",
+		description: "Mate forrado en cuero marrón con acabado en costura",
+		image:
+			"https://cdn.shopify.com/s/files/1/0630/6254/0510/products/IMG_5810_41bb5b2b-3e4b-4b52-b7f9-b5147f877db0_270x.jpg",
+		material: "Sintetico",
+		stock: 50,
+	},
+];
+
+export default orders;
