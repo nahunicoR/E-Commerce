cbb0a122cc10849d7026d2ae2356e89b4610fe00
wzseba <wzseba@gmail.com>
2022-12-31 15:45:20 -0300

    se agrega la relacion de tabla review

--- api/src/db.js
+++ api/src/db.js
@@ -6,16 +6,16 @@ const path = require("path");
 //no comentar linea debajo, sirve para pruebas locales de Back-End.
 const {  DB_USER, DB_PASSWORD, DB_HOST , DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY, {
-	logging: false, // set to console.log to see the raw SQL queries
-	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+// const sequelize = new Sequelize(DB_DEPLOY, {
+// 	logging: false, // set to console.log to see the raw SQL queries
+// 	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 
 //no eliminar linea 15, sirve para pruebas locales de Back-End.
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecomerce`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecomerce`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 const basename = path.basename(__filename);
 
@@ -43,7 +43,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { User, Order, Orderdetail, Product, Address } = sequelize.models; // Importar Modelos
+const { User, Order, Orderdetail, Product, Address, Review } = sequelize.models; // Importar Modelos
 // Aca vendrian las relaciones
 
 // Relación uno a muchos
@@ -66,6 +66,13 @@ Order.hasMany(Orderdetail, { as: "headorder", foreignkey: "id" });
 //Para relacionar al User con Address
 User.hasMany(Address, { as: "streets", foreignkey: "id" });
 //Address.belongsTo(User, { as: "user"});  //No se requiere generar una foreignkey por ya se creando el id
+
+//relaciones productos, usuario, review
+Product.hasMany(Review);
+Review.belongsTo(Product);
+
+User.hasMany(Review);
+Review.belongsTo(User);
 
 module.exports = {
 	...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
