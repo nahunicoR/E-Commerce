22378ef9429c6d61537f65e76bb45f4cf1afd23a
AlexT-Dev <atacolmx@gmail.com>
2023-01-03 15:33:01 -0600

    Lineas repetidas con error

--- api/src/db.js
+++ api/src/db.js
@@ -3,8 +3,6 @@ const { Sequelize } = require("sequelize");
 
 const fs = require("fs");
 const path = require("path");
-const {  /*DB_USER, DB_PASSWORD, DB_HOST ,*/  DB_DEPLOY } = process.env;
-
 
 //no comentar linea debajo, sirve para pruebas locales de Back-End.
 
@@ -22,14 +20,7 @@ const sequelize = new Sequelize(DB_DEPLOY, {
 	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
-const sequelize = new Sequelize(
-  DB_DEPLOY,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
-t
+
 
 const basename = path.basename(__filename);
 
