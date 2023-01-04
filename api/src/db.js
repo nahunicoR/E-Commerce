eb08a6e1e700e5c56b1dc9660ce83bb1d5c90591
nahunicoR <nahunico.r@gmail.com>
2023-01-04 10:32:55 -0300

    restaurando db.js

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 
 const fs = require("fs");
 const path = require("path");
-const {  /*DB_USER, DB_PASSWORD, DB_HOST ,*/  DB_DEPLOY } = process.env;
+const {  /*DB_USER, DB_PASSWORD, DB_HOST,*/  DB_DEPLOY } = process.env;
 
 //no comentar linea debajo, sirve para pruebas locales de Back-End.
 
