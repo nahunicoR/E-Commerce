c9cb626d6ffdbc05b6344fb1b769cf1394586cd6
AlexT-Dev <atacolmx@gmail.com>
2023-01-05 23:32:02 -0600

    Se crea proceso de carga y base de datos

--- api/src/controllers/uploadImage.js
+++ api/src/controllers/uploadImage.js
@@ -1,6 +1,8 @@
 
 //Librerías para cloudinary
 const cloudinary = require("../services/cloudConfig");
+const { Image } = require('../db');
+
 
 /* Date Creation: January 5, 2023
    Author: Alejandro Téllez Aguilar
@@ -8,23 +10,27 @@ const cloudinary = require("../services/cloudConfig");
 */
 
 //Para subir una imagen
-const  uploadImage = async (filePath) => {
-    let images = []
- 
+const  uploadImage = async (filePath, productId) => {
+    
+    console.log(productId);
     try {
       //Sube la imagen de enviada en el path, {folder: "ecommerce" } Indica la carpeta en cloudinary
       let imageUpload = await cloudinary.v2.uploader.upload(filePath,{folder: "ecommerce" })  
+      //Elimina el archivo de temmporal
+      
       //La imagen subida retorna el lugar donde está guardada la imagen y la url que se debe almacenar 
-      images.push( {
-        folder: imageUpload.public_id,
-        secure_url: imageUpload.secure_url });
-        console.log(images)
+      let newImage = await Image.create({
+         productId: productId,  
+         folder: imageUpload.public_id,
+         url: imageUpload.secure_url
+     });
+        
+        
        
-        return ({ message: "Carga completa",
-        data: images})
+        return ({ message: "Carga completa"})
      
      } catch (e) {Message : "Límite excedido, deben ser 15 MB."}  
-   
+     
  }
 
   module.exports = { uploadImage };
