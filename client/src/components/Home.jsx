41c9c437f330557a133df2bfb08ea5f1e3ac22bf
Silvia9680 <silviacarolina9680@gmail.com>
2023-01-06 15:38:54 -0300

    video de landing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -13,6 +13,7 @@ import {
 	searchProduct,
 } from "../redux/actions";
 import Filter from "./Filter";
+import { AspectRatio } from '@chakra-ui/react'
 
 export default function Home() {
 	//redux
@@ -162,6 +163,7 @@ export default function Home() {
 					</GridItem>
 				)}
 
+
 				{!loading ? (
 					<Pagination
 						pagination={pagination}
@@ -169,7 +171,14 @@ export default function Home() {
 						allProducts={products.length}
 					/>
 				) : null}
+				<AspectRatio ratio={16 / 9}>
+  <iframe
+    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng'
+  />
+</AspectRatio>
 			</Grid>
+			
+			
 		</>
 	);
 }
