ab7d8c75741d0e8c1d9e5ba88ab30e9464915cab
AlexT-Dev <atacolmx@gmail.com>
2023-01-05 23:35:48 -0600

    se modifica la forma de obtener los parámetros

--- api/src/routes/uploadImage.js
+++ api/src/routes/uploadImage.js
@@ -2,6 +2,8 @@
 const uploader = require("../services/multerCloud");
 const { Router } = require('express');
 const { uploadImage } = require('../controllers/uploadImage');
+const fs = require("fs");
+
 
 /* Date Creation: January 5, 2023
    Author: Alejandro Téllez Aguilar
@@ -10,19 +12,19 @@ const { uploadImage } = require('../controllers/uploadImage');
 
 const router = Router();
 router.post('/cloudinary/upload', uploader.array("files"), async (req,res,next) => {
-    let images = req.files
+  const { productId } = req.body; 
+     let images = req.files
     
   try {
     let imagesUploaded = [];
     console.log(req.files);
     //Para subir imagenes a claudinary
     await images.forEach(element => { // element es igual a cada imagen
-         let imageUpload = uploadImage(element.path);  //element.path, contiene la ruta y nombre de archivo
+         let imageUpload = uploadImage(element.path, productId );  //element.path, contiene la ruta y nombre de archivo
          imagesUploaded.push(imageUpload);
-         //console.log(element.path);
-         
+        
     })
-    
+ 
     return res.status(200).json({"Message: " : "Carga completada: " + imagesUploaded.length + " imagen(es)."}); 
    
   } catch (error) {Message : "Límite excedido, deben ser 15 MB."}
