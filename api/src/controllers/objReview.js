3919fc1a568021a6fb060c767f34e93748e96014
AlexT-Dev <atacolmx@gmail.com>
2023-01-05 19:57:24 -0600

    Se agregó el producId y userEmail

--- api/src/controllers/objReview.js
+++ api/src/controllers/objReview.js
@@ -2,46 +2,56 @@ module.exports = reviews =[
     {
         "description": "es un buen producto lo recomiendo",
         "rating": 5,
-        
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 1
     },
     {
         "description": "no es lo que esperaba",
         "rating": 2,
-        
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 2
     },
     {
         "description": "Estéticamente es hermoso y cumple perfectamente lo que promete",
         "rating": 4,
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 1
         
     },
     {
         "description": "Brutal , altamente recomendable",
         "rating": 5,
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 1
         
     },
     {
         "description": "es justo la bombilla que buscaba",
         "rating": 3,
-        
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 1             
     },
     {
         "description": "Es tal como dice la publicación, pero no me gusto",
         "rating": 1,
-        
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 1
     },
     {
         "description": "no me gusta para nada",
         "rating": 1,
-        
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 2
     },
     {
         "description": "lo compre solo para regalo",
         "rating": 3,
-        
+        "userEmail": "atacolmx@gmail.com",
+        "productId": 1
     },
     {
         "description": "esta muy bueno es increible",
         "rating": 5,
-        
+        "userEmail": "atacolmx@gmail.com"
     }
 ]
