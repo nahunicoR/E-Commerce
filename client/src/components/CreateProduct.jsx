0c5543e059760acaf15ea938d893e4abbfba63c0
William Salas <willbullet52@gmail.com>
2023-01-06 22:17:42 -0500

    feat(client): se parcheo el boton de creación de producto

--- client/src/components/CreateProduct.jsx
+++ client/src/components/CreateProduct.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { useToast } from "@chakra-ui/react";
 import { postProducts } from "../redux/actions";
+import { useNavigate } from "react-router-dom";
 import styles from "../css/CreateProduct.module.css";
 
 const validate = (form) => {
@@ -24,6 +26,8 @@ const validate = (form) => {
 };
 
 export default function CreateProduct() {
+	const toast = useToast();
+	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const [image, setImage] = useState("");
 	const [button, setButton] = useState(true);
@@ -63,6 +67,7 @@ export default function CreateProduct() {
 			...form,
 			[e.target.name]: e.target.value,
 		});
+
 		setErrors(
 			validate({
 				...form,
@@ -102,6 +107,7 @@ export default function CreateProduct() {
 	};
 
 	const handleSubmit = (e) => {
+		//posteo a db
 		e.preventDefault();
 		dispatch(postProducts(form));
 		console.log(form);
@@ -113,6 +119,15 @@ export default function CreateProduct() {
 			description: "",
 			image: "",
 		});
+		//feedback de creación del producto y redirección a home.
+		toast({
+			status: "success",
+			title: `${form.title} ha sido agregado a la base de datos`,
+			isClosable: true,
+		});
+		setTimeout(() => {
+			navigate("/home");
+		}, 1300);
 	};
 
 	return (
