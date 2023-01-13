5a4532e42040c0bb58aa98ff5e1dd99e0b59b0cf
William Salas <willbullet52@gmail.com>
2023-01-13 16:11:48 -0500

    map and pagination fixed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -202,7 +202,7 @@ export default function Home() {
 					>
 						<Heading size={"lg"}>Ubicanos</Heading>
 
-						<AspectRatio w={565} ratio={1 / 1}>
+						<AspectRatio w={"325%"} ratio={1 / 1}>
 							<iframe
 								title="map-stores"
 								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
