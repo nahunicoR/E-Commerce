0f766d99bc3e14e0ec4c2c3d0fdbde461e9e2f20
William Salas <willbullet52@gmail.com>
2023-02-07 14:51:37 -0500

    backend redpeloy

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 //axios.defaults.baseURL = "http://localhost:3001";
 //axios.defaults.baseURL = "https://feeb-190-84-116-240.ngrok.io";
 // axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app";
-axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app/";
+axios.defaults.baseURL = "https://mateapi.onrender.com/"; //back redeployado con render y neon.tech
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
