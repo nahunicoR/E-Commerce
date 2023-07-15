8143b2d5e0503931730744d01b18bf8e519ce5aa
nahunicoR <nahunico.r@gmail.com>
2023-07-15 11:43:41 -0300

    restoring index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,10 +12,8 @@ import { ColorModeScript } from "@chakra-ui/react";
 
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
-//axios.defaults.baseURL = "http://localhost:3001";
-//axios.defaults.baseURL = "https://feeb-190-84-116-240.ngrok.io";
-// axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app";
-axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app/";
+axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
