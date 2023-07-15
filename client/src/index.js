57b6f42f5c12e11a4e84afd0dc00bac3d4f946a1
nahunicoR <nahunico.r@gmail.com>
2023-07-15 11:52:47 -0300

    restoring part1 client index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,8 @@ import { ColorModeScript } from "@chakra-ui/react";
 
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
-axios.defaults.baseURL = "http://localhost:3001";
-// axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app/";
+// axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://e-commerce-production-9dbb.up.railway.app/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
