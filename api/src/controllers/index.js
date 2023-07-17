4eb051def4a111d4944c7045e29db50eabe5612f
nahunicoR <nahunico.r@gmail.com>
2023-07-17 13:10:00 -0300

    createReview controller,index,route

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    createReview : require('./createReview'),
     createProduct: require('./createProduct'),
     createOrder: require('./createOrder'),
     createMail: require('./createMail'),
