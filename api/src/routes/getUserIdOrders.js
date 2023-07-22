762b5b4cf044650b0fe3730546989865d7bbebc7
AlexT-Dev <atacolmx@gmail.com>
2023-07-22 12:44:22 -0600

    Se eliminaron comentarios

--- api/src/routes/getUserIdOrders.js
+++ api/src/routes/getUserIdOrders.js
@@ -1,10 +1,7 @@
 const { Router } = require('express');
 const controller = require('../controllers');
 const router = Router();
-/* Date Creation: December 13, 2022
-   Author: Alejandro Téllez Aguilar
-   Description: Crea la ruta /users/:id/orders para otener todos los usuarios
-*/
+
 router.get('/:iduser/ordenes', controller.getUserIdOrders);
 
 module.exports = router;
