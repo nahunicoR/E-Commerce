702aa231d7c7dbe423eba0487cc6d457d7a85843
AlexT-Dev <atacolmx@gmail.com>
2023-07-22 12:46:00 -0600

    Se eliminaron comentarios

--- api/src/controllers/getOrdersUsers.js
+++ api/src/controllers/getOrdersUsers.js
@@ -1,14 +1,10 @@
-/* Date Creation: December 13, 2022
-   Author: Alejandro Téllez Aguilar
-   Description: Crea la ruta /users/orders para otener todos los usuarios con sus ordenes
-*/
+
 const { User, Order } = require("../db");
 const { response } = require("../utils");
 
 module.exports = async (req,res,next) => {
     try {
         const usersDb = await User.findAll({
-            //Para traer las ordenes
             include: [{//esta llave es por si tiene más relaciones, puede quitarse si sólo es una relación como en este caso
                 model: Order,
                 as: "ordenes",
--- api/src/routes/getOrdersUsers.js
+++ api/src/routes/getOrdersUsers.js
@@ -1,10 +1,6 @@
 const { Router } = require('express');
 const controller = require('../controllers');
 
-/* Date Creation: December 13, 2022
-   Author: Alejandro Téllez Aguilar
-   Description: Crea la ruta /users/orders para otener todos los usuarios
-*/
 const router = Router();
 
 router.get('/', controller.getOrdersUsers);
