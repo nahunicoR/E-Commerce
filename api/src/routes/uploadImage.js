0d3d3655fab77a6ce8e993b23099f965d56af40c
AlexT-Dev <atacolmx@gmail.com>
2023-07-22 12:15:42 -0600

    Se hicieron cambios sólo en uploadImage

--- api/src/routes/uploadImage.js
+++ api/src/routes/uploadImage.js
@@ -4,12 +4,6 @@ const { Router } = require('express');
 const { uploadImage } = require('../controllers/uploadImage');
 const fs = require("fs");
 
-
-/* Date Creation: January 5, 2023
-   Author: Alejandro Téllez Aguilar
-   Description: Crea la ruta para el servicio para cargar imagen a cloudinary
-*/
-
 const router = Router();
 router.post('/cloudinary/upload', uploader.array("files"), async (req,res,next) => {
   const { productId } = req.body; 
@@ -18,15 +12,12 @@ router.post('/cloudinary/upload', uploader.array("files"), async (req,res,next) 
   try {
     let imagesUploaded = [];
     console.log(req.files);
-    //Para subir imagenes a claudinary
     await images.forEach(element => { // element es igual a cada imagen
          let imageUpload = uploadImage(element.path, productId );  //element.path, contiene la ruta y nombre de archivo
          imagesUploaded.push(imageUpload);
         
     })
- 
-    return res.status(200).json({"Message: " : "Carga completada: " + imagesUploaded.length + " imagen(es)."}); 
-   
+      return res.status(200).json({"Message: " : "Carga completada: " + imagesUploaded.length + " imagen(es)."}); 
   } catch (error) {Message : "Límite excedido, deben ser 15 MB."}
 });
 
