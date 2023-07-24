b5a7a7f40b76b7929e68a86f66bad66f174fc097
AlexT-Dev <atacolmx@gmail.com>
2023-07-24 09:08:30 -0600

    Se limpiaron

--- api/src/routes/getImageByProduct.js
+++ api/src/routes/getImageByProduct.js
@@ -1,9 +1,6 @@
 const { Router } = require('express');
 const controller = require('../controllers');
-/* Date Creation: January 5, 2023
-   Author: Alejandro Téllez Aguilar
-   Description: Crea la ruta /images para las imagenes por productos
-*/
+
 const router = Router();
 
 router.get('/:id/images', controller.getImageByProduct);
