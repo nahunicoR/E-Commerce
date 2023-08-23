b02950673ee5656952440ff43d3dc277d8e9d6e7
wzseba <wzseba@gmail.com>
2023-08-23 20:05:33 -0300

    refactor: se adapto codigo para carga inicial de productos

--- api/src/controllers/getProductsDb.js
+++ api/src/controllers/getProductsDb.js
@@ -1,27 +1,10 @@
 const { Product } = require("../db");
-const products = require('../controllers/objProducts');
 const {response} = require('../utils');
 
 module.exports = async (req,res,next) => {
     try {
-        const ProductosDb = await Product.findAll();
-        if (!ProductosDb.length) {
-            const load = await Product.bulkCreate(products);
-            return response(res,200,load);
-        };
-        const resp = await ProductosDb.map( product => {
-            return {
-                id: product.id,
-                title: product.title,
-                price: product.price,
-                category: product.category,
-                description: product.description,
-                image: product.image,
-                material: product.material,
-                stock: product.stock,
-            }
-        });
-        return response(res,201,resp);
+        const productosDb = await Product.findAll();
+        return response(res,201,[...productosDb]);
     } catch (error) {
         next(error);
     };
