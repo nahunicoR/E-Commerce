507750eb0844e0f11799a4ee647c815ca0b055fd
wzseba <wzseba@gmail.com>
2023-08-23 20:22:24 -0300

    refactor: se adapto codigo segun modificaciones de model Review

--- api/src/controllers/getAllReviewDB.js
+++ api/src/controllers/getAllReviewDB.js
@@ -1,64 +1,29 @@
-// const { Review } = require("../db");
-// const { User } = require('../db');
-// const { Product } = require('../db')
-
-/**CONTROLADOR PARA USAR CON BULKCREATE */
-
-// const getAllReviewDB = async (productId) => {
-   
-//     try {
-//         const reviewsDb = await Review.findAll({
-//             where:{
-//                 productId:productId,
-//                 // userEmail:userEmail
-//             }
-//         });
-
-//         let rev = await reviewsDb?.map( rev => {
-//             return {
-//                 id: rev.id,
-//                 description: rev.description,
-//                 rating: rev.rating,
-//                 productId: rev.productId,
-//                 userEmail: rev.userEmail
-//             }
-//         })
-//         return rev;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-const { Router } = require('express');
-// const  objReview  = require('../controllers/objReview');
-// const {getAllReviewDB} = require('../controllers/getAllReviewDB');
-const {Review} = require('../db');
-const {response} = require("../utils");
+const { Review } = require("../db");
+const { response } = require("../utils");
 
 module.exports = async (req, res, next) => {
-	const { id } = req.params;
-	if(parseInt(id) === 0) {
-		try {
-			const reviews = await Review.findAll({
-				attributes: {exclude: ['userEmail']},
-				order:[['id','DESC']]
-			});
-			return response(res,200,reviews);
-		} catch (error) {
-			response(res,400,error);
-		}
-	} else {
-        try {
-            const reviews = await Review.findAll({
-                where: {
-                    productId: id,	
-                },
-                attributes: {exclude: ['userEmail']},
-                order:[['id','DESC']]
-            });
-            return response(res,200,reviews);
-        } catch (error) {
-            next(error);
-        }
-	}	
-};
+   
+    try {
+		const {id} = req.params;
+		
+        const reviewsDb = await Review.findAll({
+			where:{
+				productId: id
+			},
+            order:[['id','DESC']]
+		});
+		
+        let reviews = await reviewsDb?.map( rev => {
+            return {
+                id: rev.id,
+                description: rev.description,
+                rating: rev.rating,
+                productId: rev.productId,
+                userId: rev.userId
+            }
+        })
+        return response(res,200,reviews);
+    } catch (error) {
+        next(error);
+    }
+}
