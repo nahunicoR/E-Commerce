29dee93266d646dcfaefb9381b14f43e0093c568
wzseba <wzseba@gmail.com>
2023-08-23 20:11:44 -0300

    add: se agrego primarykey

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -11,15 +11,15 @@ module.exports = (sequelize) => {
   sequelize.define('user', {
     id: {
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    name: {                            //Nombre del usuario
+    name: {
       type: DataTypes.STRING,
       allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-      primaryKey: true,
       allowNull: false,
     },
     password: {
